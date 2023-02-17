878d3f1e259229681ab8f424bf509c06e5af88e8
margarita.chekanova <margarita.chekanova@travelline.ru>
2023-02-17 16:59:18 +0300

    редактирование компонента Button

--- src/widgets/cottagesList/ui/CottagesList.tsx
+++ src/widgets/cottagesList/ui/CottagesList.tsx
@@ -17,12 +17,11 @@ interface CottagesProps {
 export const CottagesList: FC<CottagesProps> = (props) => {
 	const {className = "", title = "", list, type} = props
 
-	let roomType;
 	return (
 		<Wrapper className = {classNames([s.cottagesList, className])}>
 			<div className={s.title}>{title}</div>
 			<div className={s.list}>
-				{list.map(({name, id, description, images, price}) =>
+				{list.map(({name, id, description, images, price, roomType}) =>
 					<Apartment type={type} key={id} price={price} name={name} id={id} description={description} roomType={roomType} images={images} />
 				)}
 			</div>

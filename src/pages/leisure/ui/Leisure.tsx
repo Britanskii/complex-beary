6af056a98d57a092c73960be65f9030c60bebc75
britanskii <ohinkovskii@gmail.com>
2023-03-04 19:49:51 +0400

    add: leisure slider

--- src/pages/leisure/ui/Leisure.tsx
+++ src/pages/leisure/ui/Leisure.tsx
@@ -5,9 +5,24 @@ import {FC} from "react"
 import {classNames} from "shared/lib/classNames/classNames"
 import {Wrapper} from "widgets"
 
-import snow from "../assets/snow.jpg"
-import church from "../assets/church.png"
-import fishing from "../assets/fishing.jpg"
+import snow1 from "../assets/snow/1.webp"
+import snow2 from "../assets/snow/2.webp"
+import snow3 from "../assets/snow/3.webp"
+import snow4 from "../assets/snow/4.webp"
+import snow5 from "../assets/snow/5.webp"
+
+import excursion1 from "../assets/excursions/1.png"
+import excursion2 from "../assets/excursions/2.jpeg"
+import excursion3 from "../assets/excursions/3.jpg"
+import excursion4 from "../assets/excursions/4.jpg"
+import excursion5 from "../assets/excursions/5.jpg"
+
+import fishing1 from "../assets/fishing/1.webp"
+import fishing2 from "../assets/fishing/2.webp"
+import fishing3 from "../assets/fishing/3.webp"
+import fishing4 from "../assets/fishing/4.webp"
+import fishing5 from "../assets/fishing/5.webp"
+import {Slider, SliderVariants} from "shared/ui/sliderNew/ui/Slider"
 
 interface LeisureProps {
     className?: string
@@ -16,7 +31,7 @@ interface LeisureProps {
 interface Leisure {
 	title: string,
 	description: string,
-	image: string
+	images: string[]
 }
 
 const data: Leisure[] = [
@@ -27,7 +42,7 @@ const data: Leisure[] = [
 		description: "Отличное место для отдыха. В Онежских водах и близлежащих лесных озерах водится приличное количество рыбы, \n" +
 			"в здешних местах удачливому рыбаку \n" +
 			"или охотнику не придется скучать. Всем необходимым для промысла Вы также сможете снабдиться прямо у нас.",
-		image: fishing
+		images: [fishing1, fishing2, fishing3, fishing4, fishing5]
 	},
 	{
 		title: "Активный отдых",
@@ -37,7 +52,7 @@ const data: Leisure[] = [
 			"В 5 киломметрах расположен горнолыжный комплекс: 4 склона от 250 \n" +
 			"до 420 метров.Ресторан \"Медвежка\" \n" +
 			"из сухостойной сосны ручной работы (Единственный на северо-западе), на 120 мест. Русские бани, сауна с шуньгитовым бассейном. (шуньгит - целебный минерал из Карелии)",
-		image: snow
+		images: [snow1, snow2, snow3, snow4, snow5]
 	},
 	{
 		title: "Экскурсионные программы",
@@ -46,7 +61,7 @@ const data: Leisure[] = [
 			"Музей-заповедник «КИЖИ», Водопад «КИВАЧ», Соловецкие острова находятся \n" +
 			"на самом близком расстоянии от «Малой Медвежки» и Вы сможете все их посетить \n" +
 			"во время отдыха.",
-		image: church
+		images: [excursion1, excursion2, excursion3, excursion4, excursion5]
 	}
 ]
 
@@ -57,9 +72,9 @@ export const Leisure: FC<LeisureProps> = (props) => {
 		<Wrapper className = {classNames([s.leisure, className])}>
 			<h2 className={s.title}>Отдых и развлечения</h2>
 			<div className={s.flex}>
-				{data.map(({title, description, image}, index) =>
+				{data.map(({title, description, images}, index) =>
 					<div className={s.item} key={index}>
-						<img className={s.image} src={image}/>
+						<Slider variant={SliderVariants.CLEAR} className={s.image} images={images}/>
 						<div className={s.text}>
 							<div className={s.subtitle}>
 								{title}
